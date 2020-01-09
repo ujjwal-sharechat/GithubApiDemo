8ab6d6123d65fc9d401954faf2f2d8a8f3191a7e
ujjwal-sharechat <ujjwal@sharechat.co>
2020-01-09 16:37:39 +0530

    MVP for Repoactivity

--- app/src/main/java/com/example/githubapidemo/Repositories.kt
+++ app/src/main/java/com/example/githubapidemo/Repositories.kt
@@ -1,11 +1,14 @@
 package com.example.githubapidemo
 
+import Present.ContractRepo
 import adapter.ReposAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.githubapidemo.PresenterClasses.RepoPresenter
+import com.example.githubapidemo.PresenterClasses.UserPresenter
 import com.example.githubapidemo.model.GitHubUsers
 import com.example.githubapidemo.model.GithubRepo
 import com.example.githubapidemo.rest.ApiClient
@@ -16,7 +19,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class Repositories : AppCompatActivity() {
+class Repositories : AppCompatActivity(),ContractRepo.RepoInterface {
 
 
     lateinit var recievedusername : String
@@ -34,8 +37,12 @@ class Repositories : AppCompatActivity() {
         }
         userNameTV.text= "USER : $recievedusername"
 
-        loadrepositories()
+       // loadrepositories()
 
+
+        val userobject = RepoPresenter(this)
+
+        userobject.fetchdata(recievedusername)
 
     repos_recycler_view.layoutManager = LinearLayoutManager(this)
         mAdapter = ReposAdapter(mdatasource)
@@ -45,28 +52,35 @@ class Repositories : AppCompatActivity() {
 
     }
 
-    fun loadrepositories()
-    {
-        val apiservice : GithubRepoEndPoint = ApiClient.getClient()!!.create(GithubRepoEndPoint::class.java)
+//    fun loadrepositories()
+//    {
+//        val apiservice : GithubRepoEndPoint = ApiClient.getClient()!!.create(GithubRepoEndPoint::class.java)
+//
+//        var call : Call<List<GithubRepo>> = apiservice.getRepo(recievedusername)
+//
+//        call.enqueue(object : Callback<List<GithubRepo>> {
+//            override fun onFailure(call: Call<List<GithubRepo>>, t: Throwable) {
+//                Log.d("repos",t.toString())
+//            }
+//
+//            override fun onResponse(call: Call<List<GithubRepo>>, response: Response<List<GithubRepo>>) {
+//                mdatasource.clear()
+//                response.body()?.let {
+//                    mdatasource.addAll(it)
+//                    mAdapter?.update(mdatasource)
+//                }
+//
+//                repos_recycler_view.adapter?.notifyDataSetChanged()
+//            }
+//
+//        })
+//
+//    }
 
-        var call : Call<List<GithubRepo>> = apiservice.getRepo(recievedusername)
-
-        call.enqueue(object : Callback<List<GithubRepo>> {
-            override fun onFailure(call: Call<List<GithubRepo>>, t: Throwable) {
-                Log.d("repos",t.toString())
-            }
-
-            override fun onResponse(call: Call<List<GithubRepo>>, response: Response<List<GithubRepo>>) {
-                mdatasource.clear()
-                response.body()?.let {
-                    mdatasource.addAll(it)
-                    mAdapter?.update(mdatasource)
-                }
-
-                repos_recycler_view.adapter?.notifyDataSetChanged()
-            }
-
-        })
-
+    override fun setdata(mdatasource: ArrayList<GithubRepo>) {
+        mAdapter?.update(mdatasource)
+        repos_recycler_view.adapter?.notifyDataSetChanged()
     }
+
+
 }
